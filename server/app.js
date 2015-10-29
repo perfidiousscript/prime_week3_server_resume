fe4e22296125d5285c69cae78e9adaf21f39a8b5
Samuel Moss <samuelmoss@Samuels-MacBook-Pro.local>
2015-10-29 17:02:04 -0500

    functional server

--- server/app.js
+++ server/app.js
@@ -1,2 +1,15 @@
 var express = require('express');
-var app - express();
+var app = express();
+var path = require('path');
+
+app.set("port",process.env.PORT || 5000);
+
+app.get("/*",function(req,res){
+    console.log(req.params);
+    var file = req.params[0] || "index.html";
+    res.sendFile(path.join(__dirname,"../Public",file));
+});
+
+app.listen(app.get("port"), function(){
+    console.log("Listening on port: ", app.get("port"));
+});
